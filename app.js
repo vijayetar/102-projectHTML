d34b29706a4847aa0b613bfa84e37289f299a25d
Vijayeta Rangarajan <vijayetar@gmail.com>
2019-10-18 11:00:53 -0700

    more loops

--- app.js
+++ app.js
@@ -30,37 +30,30 @@ function choiceCakes() {
 }
 
 function selectService() {
-    var selectCakes = prompt('What cake would you like to order - birthday/wedding/specialty?');
-    if (selectCake === 'birthday' || selectCake === 'birthday' || selectCake === 'specialty') {
-    var numberCakes = prompt('How many cakes would you like to order?');
-    while (numberCakes === '' || isNaN(numberCakes)) {
-        var numberCakes = prompt('Great, how many cakes would you like to order? PLEASE enter a number');
-    return numberCakes;
-    }
-    else {
-        selectClasses()
-    }
-    
-}
-
-function getCartoon() {
-    var cartoon;
-    cartoon = '<img src="images/cartoon_cake.png">';
-    for (var i = 0; i < numberCakes; i++) {
-        result = result + '<p id = "cartoon">Cake no.' + (i + 1) + '' + cartoon + '</p>';
-    }
-    return '<h3>' + 'You ordered' + '' + numberCakes + '' + 'cakes! ' + '</h3>';
-}
-
-function selectClasses() {
     var askClasses = prompt('Would you like to check out our classes? (Y/N)');
     var msgClasses;
-
     if (askClasses === 'yes' || askClasses === 'Y' || askClasses === 'y') {
         msgClasses = 'Great! Please click the classes image to check out our pricing';
     } else {
-        msgClasses = 'Great! Please visit our site any time';
+        var selectCake = prompt('What cake would you like to order - birthday/wedding/specialty?');
+        if (selectCake === 'birthday' || selectCake === 'wedding' || selectCake === 'specialty') {
+            var numberCakes = prompt('How many cakes would you like to order?');
+                while (numberCakes === '' || isNaN(numberCakes)) {
+                var numberCakes = prompt('Great, how many cakes would you like to order? PLEASE enter a number');
+                }
+            return numberCakes;    
+            msgClasses = 'You ordered' + numberCakes + 'today' + '!';
+        } else {
+            msgClasses = 'Thank you for visiting our site!';
+        }
     }
-    return '<p>' + msgClasses + '</p>';
+    return '<p>' + msgClasses + '!' + '</p>';
 }
+
+function getCartoon() {
+    var cartoon = '<img src="images/cartoon_cake.png">';
+    for (var i = 0; i < numberCakes; i++) {
+        result = result + '<p id = "cartoon"> Cake no.' + (i + 1) + '' + cartoon + '</p>';
+    }
+    return '<p>' + 'You ordered' + '' + numberCakes + '' + 'cakes! ' + '</p>';
 }
